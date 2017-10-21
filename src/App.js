fc7aa3da1a4b9ce3a5a3b46b12f88ce4de969ab7
Chris-Maina <krismaish34@gmail.com>
2017-10-21 14:02:51 +0300

    [Chore #[152006358]] Add Switch component and refactor Route paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Route} from 'react-router-dom';
+import { Route, Switch} from 'react-router-dom';
 /** Import all pages components */
 import RegisterPage from './components/register';
 import LoginPage from './components/login';
 import ShoppinglistPage from './components/shoppinglist';
-import ShoppingItemsPage from './components/shoppingitem';
 import Home from './components/home';
+import ShoppingItemsPage from './components/shoppingitem';
 import './App.css';
 
 class App extends Component {
@@ -16,12 +16,13 @@ class App extends Component {
             Define path for the other pages:
             Register, Login, Shopping list, Shopping items
           */}
-
+        <Switch>
         <Route exact={true} path="/" component={Home} />
         <Route path="/auth/register/" component={RegisterPage} />
         <Route path="/auth/login/" component={LoginPage} />
-        <Route path="/shoppinglist/" component={ShoppinglistPage} />
-        <Route path="/items/:id" component={ShoppingItemsPage} />
+        <Route exact ={true} path="/shoppinglists/" component={ShoppinglistPage} />
+        <Route path={`/shoppinglists/:sl_id/items`} component={ShoppingItemsPage} />
+        </Switch>
       </div>
     );
   }
