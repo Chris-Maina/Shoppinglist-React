0f17c0d04eba8d809ec07ac952c7769ac7663b31
Chris-Maina <krismaish34@gmail.com>
2017-10-21 10:43:05 +0300

    [Chore #[152006358]] Define routes to components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import { Route} from 'react-router-dom';
+/** Import all pages components */
+import RegisterPage from './components/register';
+import LoginPage from './components/login';
+import ShoppinglistPage from './components/shoppinglist';
+import ShoppingItemsPage from './components/shoppingitem';
 import Home from './components/home';
 import './App.css';
 
@@ -6,7 +12,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Home />
+        {/* Entry point App
+            Define path for the other pages:
+            Register, Login, Shopping list, Shopping items
+          */}
+
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/auth/register/" component={RegisterPage} />
+        <Route path="/auth/login/" component={LoginPage} />
+        <Route path="/shoppinglist/" component={ShoppinglistPage} />
+        <Route path="/items/:id" component={ShoppingItemsPage} />
       </div>
     );
   }
