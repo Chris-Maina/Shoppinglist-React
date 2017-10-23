fbc5031dca077d3aa79d3e3aa88fc408c5437cc3
Chris-Maina <krismaish34@gmail.com>
2017-10-23 12:18:19 +0300

    [Feature #[151155294]] Add handleLogoutClick function to logout users

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
-import { Navbar, NavItem} from 'react-materialize';
+import { Redirect } from 'react-router-dom';
+import { Navbar, NavItem , Dropdown} from 'react-materialize';
 // Navigation bar
 class Navigation extends Component {
+  constructor(props){
+    super(props);
+    this.state = {redirect: false};
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+  }
+  handleLogoutClick(){
+    window.localStorage.removeItem('token');
+    this.setState({redirect: true});
+  }
   render() {
+    if(this.state.redirect){
+      return(
+        <Redirect to="/auth/login/"/>
+      );
+    }
     return (
       <div>
         <Navbar brand='SHOPPINGLIST' right>
-          <NavItem >Home</NavItem>
-          <NavItem>Register</NavItem>
-          <NavItem href='/auth/login/'>Login</NavItem>
-          <NavItem href='/shoppinglist'>My Lists</NavItem>
+          <NavItem href='/shoppinglists/'>My Lists</NavItem>
+          <Dropdown trigger={
+            <NavItem >User</NavItem>
+          }>
+            <NavItem href='#'>Profile</NavItem>
+            <NavItem  onClick={this.handleLogoutClick}>logout</NavItem>
+          </Dropdown>
         </Navbar>
       </div>
     );
