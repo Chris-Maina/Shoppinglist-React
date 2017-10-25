fda40b72046d2ad693adfc7d453e761442011f62
Chris-Maina <krismaish34@gmail.com>
2017-10-25 21:39:53 +0300

    [Feature #[152290524]] Add methods to handle shopping list searching

--- src/components/shoppinglist.js
+++ src/components/shoppinglist.js
@@ -23,6 +23,8 @@ class ShoppinglistPage extends Component {
         this.deleteShoppinglist = this.deleteShoppinglist.bind(this);
         this.handleEditShoppinglist = this.handleEditShoppinglist.bind(this);
         this.editShoppinglist = this.editShoppinglist.bind(this);
+        this.searchShoppinglist = this.searchShoppinglist.bind(this);
+        this.handleSearchShoppinglist = this.handleSearchShoppinglist.bind(this);
 
     }
     componentDidMount() {
@@ -42,7 +44,6 @@ class ShoppinglistPage extends Component {
             if (!response.statusText === 'OK') {
                 toast.error(response.data.message)
             }
-
             console.log(response.data.shopping_lists);
             this.setState({
                 shoppinglists: response.data.shopping_lists,
@@ -62,7 +63,7 @@ class ShoppinglistPage extends Component {
                 console.log(error.request);
             } else {
                 // Something happened in setting up the request that triggered an Error
-                console.log('Error', error.message);
+                console.log('Error', JSON.stringify(error.message));
             }
             console.log(error.config);
         });
@@ -173,6 +174,8 @@ class ShoppinglistPage extends Component {
             }
             console.log(response.data);
             toast.success("Shoppinglist " + response.data.name + " created");
+            // Get ALL shopping list
+            this.getShoppinglists();
             return response.data;
         }).catch(function (error) {
             if (error.response) {
@@ -189,52 +192,123 @@ class ShoppinglistPage extends Component {
             }
             console.log(error.config);
         });
-        // Get ALL shopping list
-        this.getShoppinglists();
+    }
+    handleSearchShoppinglist(searchText){
+        this.searchShoppinglist(searchText);
+    }
+    searchShoppinglist(searchText){
+        // Send GET request
+        const url = 'https://shoppinglist-restful-api.herokuapp.com/shoppinglists/?q='+ searchText;
+        axios({
+            method: "get",
+            url: url,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + window.localStorage.getItem('token')
+            }
+        }).then((response) => {
+            if (!response.statusText === 'OK') {
+                toast.error(response.data.message)
+            }
+
+            console.log(response.data);
+            this.setState({
+                shoppinglists: response.data,
+            });
+
+            return response.data;
+        }).catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                toast.error(error.response.data.message)
+            } else if (error.request) {
+                // The request was made but no response was received
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+            console.log(error.config);
+        });
     }
 
     render() {
         return (
             <div>
                 <Navigation />
-            <div className="pagecontent">
-                <Container  >
-                    <ToastContainer />
+                <div className="pagecontent">
+                    <Container  >
+                        <ToastContainer />
 
-                    <ToggleableShoppingForm
-                        formSubmit={this.handelShoppinglistNameSubmit} />
-                    <AllShoppinglists
-                        shopping_lists={this.state.shoppinglists}
-                        editFormSubmit={this.handleEditShoppinglist}
-                        deleteSubmit={this.handleDeleteShoppinglist} />
-                    <NextPreviousPage
-                        next_page={this.state.next_page}
-                        prev_page={this.state.previous_page} />
-                </Container>
-            </div>
+                        <ToggleableShoppingForm
+                            formSubmit={this.handelShoppinglistNameSubmit}
+                            onSearchSubmit={this.handleSearchShoppinglist} />
+                        <AllShoppinglists
+                            shopping_lists={this.state.shoppinglists}
+                            editFormSubmit={this.handleEditShoppinglist}
+                            deleteSubmit={this.handleDeleteShoppinglist} />
+                        <NextPreviousPage
+                            next_page={this.state.next_page}
+                            prev_page={this.state.previous_page} />
+                    </Container>
+                </div>
             </div>
         );
     }
 }
 class NextPreviousPage extends Component {
     render() {
+        if (this.props.prev_page !== "None") {
+            return (
+                <Row>
+                    <Col xs="8" xs-offset="2" md="8" md-offset="2">
+                        <Button className='orange' waves='light' size="small" ><Link to={this.props.prev_page}>Previous Page</Link></Button>
+                    </Col>
+                </Row>
+            );
+        }
+        else if (this.props.next_page !== 'None') {
+            return (
+                <Row>
+                    <Col xs="8" xs-offset="2" md="8" md-offset="2">
+
+                        <Button className='orange' waves='light' size="small" ><Link to={this.props.next_page}>Next Page</Link></Button>
+                    </Col>
+                </Row>
+            );
+        }
+        else if (this.props.prev_page !== 'None' & this.props.next_page !== 'None') {
+            return (
+                <Row>
+                    <Col xs="8" xs-offset="2" md="8" md-offset="2">
+                        <Button className='orange' waves='light' size="small" ><Link to={this.props.prev_page}>Previous Page</Link></Button>
+                        <Button className='orange' waves='light' size="small" ><Link to={this.props.next_page}>Next Page</Link></Button>
+                    </Col>
+                </Row>
+            );
+        }
         return (
             <Row>
                 <Col xs="8" xs-offset="2" md="8" md-offset="2">
-                    <Button className='orange' waves='light' size="small" ><Link to={this.props.next_page}>Next Page</Link></Button>
-
-                    <Button className='orange' waves='light' size="small" ><Link to={this.props.prev_page}>Previous Page</Link></Button>
+                    <Button className='orange' waves='light' size="small" >Previous Page</Button>
+                    <Button className='orange space' waves='light' size="small" >Next Page</Button>
                 </Col>
-            </Row>);
+            </Row>
+        );
     }
 }
 class ToggleableShoppingForm extends Component {
     constructor(props) {
         super(props);
-        this.state = { isOpen: false };
+        this.state = { isOpen: false, isSearchOpen: false };
         this.handleFormOpen = this.handleFormOpen.bind(this);
         this.handleFormClose = this.handleFormClose.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.handleSearchOpen = this.handleSearchOpen.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleSearchClose = this.handleSearchClose.bind(this);
     }
     handleFormSubmit(shoppinglistname) {
         this.props.formSubmit(shoppinglistname);
@@ -246,20 +320,40 @@ class ToggleableShoppingForm extends Component {
     handleFormOpen() {
         this.setState({ isOpen: true });
     }
+    handleSearchOpen() {
+        this.setState({ isSearchOpen: true });
+    }
+    handleSearchClose() {
+        this.setState({ isSearchOpen: false });
+    }
+    handleSearch(searchText){
+        this.props.onSearchSubmit(searchText);
+        this.setState({ isSearchOpen: false });
+    }
     render() {
         const isOpen = this.state.isOpen;
+        const isSearchOpen = this.state.isSearchOpen;
         if (isOpen) {
             return (
                 <ShoppinglistForm
                     onFormSubmit={this.handleFormSubmit}
                     onFormClose={this.handleFormClose} />
             );
-        } else {
+        } else if (isSearchOpen) {
+            return (
+                <SearchShoppinglist
+                    onCancelClick={this.handleSearchClose}
+                    onSearchSubmit={this.handleSearch} />
+            );
+        }
+        else {
             return (
                 <Row>
                     <Col xs="8" xs-offset="2" md="8" md-offset="2">
                         <div>
                             <Button floating large className='orange' waves='light' icon='add' onClick={this.handleFormOpen} />
+                            
+                            <Button floating large className='orange space' waves='light' icon='search' onClick={this.handleSearchOpen} />
                         </div>
                     </Col>
                 </Row>
@@ -359,7 +453,7 @@ class Shoppinglist extends Component {
                                 actions={[<Link to={`/shoppinglists/${this.props.sl_id}/items`}>Add Item</Link>]}>
 
                                 <br />
-                                <Button color="primary" size="small" onClick={this.props.onEditSubmit}>Edit</Button>
+                                <Button color="primary" size="small" onClick={this.props.onEditSubmit}>Edit</Button>&nbsp;&nbsp;&nbsp;&nbsp;
                                 <Button className="red" size="small" onClick={this.props.onDeleteSubmit}>Delete</Button>
                             </Card>
                         </div>
@@ -405,4 +499,40 @@ class ShoppinglistForm extends Component {
         );
     }
 }
+class SearchShoppinglist extends Component {
+    constructor(props){
+        super(props);
+        this.state = { searchText: '' };
+        this.handleSearch = this.handleSearch.bind(this);
+        this.onSearchInputChange = this.onSearchInputChange.bind(this);
+    }
+    componentDidMount(){
+        this.setState({ searchText: this.props.searchText });
+    }
+    onSearchInputChange(evt){
+        evt.preventDefault();
+        this.setState({ searchText: evt.target.value });
+    }
+    handleSearch(evt){
+        evt.preventDefault();
+        this.props.onSearchSubmit(this.state.searchText);
+    }
+    render() {
+        return (
+            <Row>
+                <Col xs="8" xs-offset="2" md="8" md-offset="2">
+                    <div>
+                        <Form onSubmit={this.handleSearch}>
+                            <Input label="Search shoppinglist" floatingLabel={true} type="text"  name='searchtext' value={this.state.searchText} onChange={this.onSearchInputChange}></Input>
+                            <Button color="primary" size="small" onClick={this.handleSearch}>Search</Button>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <Button className="red" size="small" onClick={this.props.onCancelClick}>Cancel</Button>
+                        </Form>
+                    </div>
+                </Col>
+            </Row>
+        );
+    }
+}
+
 export default ShoppinglistPage;
