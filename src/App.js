01993adc708d8b34c7fe029211ef79621ed3be35
Chris-Maina <krismaish34@gmail.com>
2017-10-28 19:42:43 +0300

    [Feature #[152374386]] Add UserProfile component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { Route, Switch} from 'react-router-dom';
 /** Import all pages components */
 import RegisterPage from './components/register';
-import LoginPage from './components/login';
+import LoginForm from './components/login';
 import ShoppinglistPage from './components/shoppinglist';
 import Home from './components/home';
 import ShoppingItemsPage from './components/shoppingitem';
+import UserProfile from './components/user_profile';
 import './App.css';
 
 class App extends Component {
@@ -18,8 +19,9 @@ class App extends Component {
           */}
         <Switch>
         <Route exact={true} path="/" component={Home} />
+        <Route exact={true} path="/user" component={UserProfile} />
         <Route path="/auth/register/" component={RegisterPage} />
-        <Route path="/auth/login/" component={LoginPage} />
+        <Route path="/auth/login/" component={LoginForm} />
         <Route exact ={true} path="/shoppinglists/" component={ShoppinglistPage} />
         <Route path={`/shoppinglists/:sl_id/items`} component={ShoppingItemsPage} />
         </Switch>
