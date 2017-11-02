1e42020b6c45452fda2cb61f13f18ea6135cf136
Chris-Maina <krismaish34@gmail.com>
2017-11-02 15:37:33 +0300

    [Feature #[152374433]] Refactor importation of RegisterPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Route, Switch} from 'react-router-dom';
 /** Import all pages components */
-import RegisterPage from './components/register';
+import { RegisterPage } from './components/register';
 import LoginForm from './components/login';
 import ShoppinglistPage from './components/shoppinglist';
 import Home from './components/home';
 import ShoppingItemsPage from './components/shoppingitem';
 import UserProfile from './components/user_profile';
+import ForgotPassword from './components/forgot_password';
 import './App.css';
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
         <Switch>
         <Route exact={true} path="/" component={Home} />
         <Route exact={true} path="/user" component={UserProfile} />
+        <Route exact={true} path="/user/reset" component={ForgotPassword} />
         <Route path="/auth/register/" component={RegisterPage} />
         <Route path="/auth/login/" component={LoginForm} />
         <Route exact ={true} path="/shoppinglists/" component={ShoppinglistPage} />
