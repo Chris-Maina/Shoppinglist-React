9ab1a457c1cf1e4502e2e8a0e13e510e906b183e
Chris-Maina <krismaish34@gmail.com>
2017-11-04 18:37:05 +0300

    [Chore #[152374386]] Add moxios stubResquest and wait function

--- src/__tests__/register.test.js
+++ src/__tests__/register.test.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
+import moxios from 'moxios';
+import axios from 'axios';
 import ReactDOM from 'react-dom';
 import {RegisterPage, RegisterForm} from '../components/register';
 
@@ -8,8 +11,6 @@ describe('<RegisterPage/> component', () => {
         const registerPage = shallow(<RegisterPage/>);
         expect(registerPage).toHaveLength(1);
     });
-});
-describe('<RegisterForm/> component', () => {
     it('Renders RegisterForm component by default',() =>{
         const registerPage = shallow(<RegisterPage/>);
         const registerForm = registerPage.find('RegisterForm');
@@ -19,12 +20,27 @@ describe('<RegisterForm/> component', () => {
         const registerForm = shallow(<RegisterForm title="Register"/>);
         expect(registerForm.instance().props.title).toBe('Register');
     });
-    // it('Redirects on when state of redirect is True ',() => {
-    //     const registerForm = shallow(<RegisterPage/>);
-    //     registerForm.setState({redirect: true});
-    //     const loginPage = registerForm.find('LoginForm');
-    //     expect(loginPage).toHaveLength(1);
-    // });
+});
+describe('Form Validation in Register page', () => {
+    beforeEach( function () {
+        moxios.install()
+    })
+    afterEach( function () {
+        moxios.uninstall()
+    })
+    it('Submits to the user details to API server', () => {
+
+        moxios.stubRequest('https://shoppinglist-restful-api.herokuapp.com/auth/register/', {
+            status: 200,
+            responseText: "Successful registration"
+        })
+        let onRegister = sinon.spy()
+        axios.post('https://shoppinglist-restful-api.herokuapp.com/auth/register/').then(onRegister)
+        moxios.wait( function () {
+            expect(onRegister.getCall(0).args[0].data).toBe('Successful registration');
+            done()
+        })
+    });
     it('Changes state on form submission ',() => {
         const registerForm = mount(<RegisterForm/>);
         registerForm.setState({username: 'chris', email: 'chris@gmail.com', password: 'chris1234', cpassword: 'chris1234'});
@@ -32,9 +48,7 @@ describe('<RegisterForm/> component', () => {
         pageForm.simulate('submit')
         expect(registerForm.state().username).toBe('');
     });
-    
-});
-describe('Form Validation in Register page', () => {
+
     it('Raises an error when password dont match',() => {
         const registerForm = mount(<RegisterForm/>);
         registerForm.setState({username: 'chris', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'chris1234'});
@@ -55,5 +69,21 @@ describe('Form Validation in Register page', () => {
         const pageForm = registerForm.find('Form')
         pageForm.simulate('submit')
         expect(registerForm.state().errors).toBe('Please provide a username');
+    });
+    it('Calls sendRequest when form is submitted ',()=> {
+        sinon.spy(RegisterForm.prototype, 'sendRequest');
+        const wrapper = mount(<RegisterForm/>);
+        wrapper.setState({username: 'chris', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'maina1234'});
+        const pageForm = wrapper.find('Form')
+        pageForm.simulate('submit')
+        expect(RegisterForm.prototype.sendRequest.calledOnce).toEqual(true);
+    });
+    it('Calls validate when form is submitted ',()=> {
+        sinon.spy(RegisterForm.prototype, 'validate');
+        const wrapper = mount(<RegisterForm/>);
+        wrapper.setState({username: 'chris', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'maina1234'});
+        const pageForm = wrapper.find('Form')
+        pageForm.simulate('submit')
+        expect(RegisterForm.prototype.validate.calledOnce).toEqual(true);
     });
 });
