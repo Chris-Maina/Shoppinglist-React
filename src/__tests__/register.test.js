656a468b374dd3ac3996630a94668dcbb166d9cf
Chris-Maina <krismaish34@gmail.com>
2017-11-04 19:31:10 +0300

    [Chore #[152374386]] Add Raises error when email and password are empty test cases

--- src/__tests__/register.test.js
+++ src/__tests__/register.test.js
@@ -4,84 +4,127 @@ import sinon from 'sinon';
 import moxios from 'moxios';
 import axios from 'axios';
 import ReactDOM from 'react-dom';
-import {RegisterPage, RegisterForm} from '../components/register';
+import { RegisterPage, RegisterForm } from '../components/register';
 
 describe('<RegisterPage/> component', () => {
-    it('Renders RegisterPage component',() =>{
-        const registerPage = shallow(<RegisterPage/>);
+    it('Renders RegisterPage component', () => {
+        const registerPage = shallow(<RegisterPage />);
         expect(registerPage).toHaveLength(1);
     });
-    it('Renders RegisterForm component by default',() =>{
-        const registerPage = shallow(<RegisterPage/>);
+    it('Renders RegisterForm component by default', () => {
+        const registerPage = shallow(<RegisterPage />);
         const registerForm = registerPage.find('RegisterForm');
         expect(registerForm).toHaveLength(1);
     });
-    it('Renders props correctly',() =>{
-        const registerForm = shallow(<RegisterForm title="Register"/>);
+    it('Renders props correctly', () => {
+        const registerForm = shallow(<RegisterForm title="Register" />);
         expect(registerForm.instance().props.title).toBe('Register');
     });
 });
 describe('Form Validation in Register page', () => {
-    beforeEach( function () {
+    let onFulfilled
+    let onRejected
+    beforeEach(function () {
         moxios.install()
+        onFulfilled = sinon.spy()
+        onRejected = sinon.spy()
     })
-    afterEach( function () {
+    afterEach(function () {
         moxios.uninstall()
     })
-    it('Submits to the user details to API server', () => {
-
+    it('Submits user details to API server, registration success', () => {
         moxios.stubRequest('https://shoppinglist-restful-api.herokuapp.com/auth/register/', {
             status: 200,
             responseText: "Successful registration"
         })
-        let onRegister = sinon.spy()
-        axios.post('https://shoppinglist-restful-api.herokuapp.com/auth/register/').then(onRegister)
-        moxios.wait( function () {
-            expect(onRegister.getCall(0).args[0].data).toBe('Successful registration');
+        axios.post('https://shoppinglist-restful-api.herokuapp.com/auth/register/', {
+            email: 'chris@gmail.com',
+            password: 'chris1234'
+        }).then(onFulfilled)
+        moxios.wait(function () {
+            expect(onFulfilled.getCall(0).args[0].data).toBe('Successful registration');
             done()
         })
     });
-    it('Changes state on form submission ',() => {
-        const registerForm = mount(<RegisterForm/>);
-        registerForm.setState({username: 'chris', email: 'chris@gmail.com', password: 'chris1234', cpassword: 'chris1234'});
+    it('Raises error when email is not submited', () => {
+
+        axios.post('https://shoppinglist-restful-api.herokuapp.com/auth/register/', {
+            email: '',
+            password: 'chris1234'
+        }).then(onFulfilled, onRejected)
+
+        moxios.wait(function () {
+            let request = moxios.requests.mostRecent()
+            request.respondWith({
+                status: 400,
+                response: "Error"
+            }).then(function () {
+                expect(onRejected.getCall(0).args[0].data).toBe('Error');
+                done()
+            })
+        })
+
+    })
+    it('Raises error when password is not submited', () => {
+
+        axios.post('https://shoppinglist-restful-api.herokuapp.com/auth/register/', {
+            email: 'chris@gmail.com',
+            password: ''
+        }).then(onFulfilled, onRejected)
+
+        moxios.wait(function () {
+            let request = moxios.requests.mostRecent()
+            request.respondWith({
+                status: 400,
+                response: "Error"
+            }).then(function () {
+                expect(onRejected.getCall(0).args[0].data).toBe('Error');
+                done()
+            })
+        })
+
+    })
+    it('Changes state on form submission ', () => {
+        const registerForm = mount(<RegisterForm />);
+        registerForm.setState({ username: 'chris', email: 'chris@gmail.com', password: 'chris1234', cpassword: 'chris1234' });
         const pageForm = registerForm.find('Form')
         pageForm.simulate('submit')
         expect(registerForm.state().username).toBe('');
     });
 
-    it('Raises an error when password dont match',() => {
-        const registerForm = mount(<RegisterForm/>);
-        registerForm.setState({username: 'chris', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'chris1234'});
+    it('Raises an error when password dont match', () => {
+        const registerForm = mount(<RegisterForm />);
+        registerForm.setState({ username: 'chris', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'chris1234' });
         const pageForm = registerForm.find('Form')
         pageForm.simulate('submit')
         expect(registerForm.state().errors).toBe('Password mismatch');
     });
-    it('Raises an error when username has special characters',() => {
-        const registerForm = mount(<RegisterForm/>);
-        registerForm.setState({username: 'chris+*', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'maina1234'});
+    it('Raises an error when username has special characters', () => {
+        const registerForm = mount(<RegisterForm />);
+        registerForm.setState({ username: 'chris+*', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'maina1234' });
         const pageForm = registerForm.find('Form')
         pageForm.simulate('submit')
         expect(registerForm.state().errors).toBe('Username cannot have special characters');
     });
-    it('Raises an error when username is empty',() => {
-        const registerForm = mount(<RegisterForm/>);
-        registerForm.setState({username: '', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'maina1234'});
+    it('Raises an error when username is empty', () => {
+        const registerForm = mount(<RegisterForm />);
+        registerForm.setState({ username: '', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'maina1234' });
         const pageForm = registerForm.find('Form')
         pageForm.simulate('submit')
         expect(registerForm.state().errors).toBe('Please provide a username');
     });
-    it('Calls sendRequest when form is submitted ',()=> {
+    it('Calls sendRequest when form is submitted ', () => {
         sinon.spy(RegisterForm.prototype, 'sendRequest');
-        const wrapper = mount(<RegisterForm/>);
-        wrapper.setState({username: 'chris', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'maina1234'});
+        const wrapper = mount(<RegisterForm />);
+        wrapper.setState({ username: 'chris', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'maina1234' });
         const pageForm = wrapper.find('Form')
         pageForm.simulate('submit')
         expect(RegisterForm.prototype.sendRequest.calledOnce).toEqual(true);
     });
-    it('Calls validate when form is submitted ',()=> {
+    it('Calls validate when form is submitted ', () => {
         sinon.spy(RegisterForm.prototype, 'validate');
-        const wrapper = mount(<RegisterForm/>);
-        wrapper.setState({username: 'chris', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'maina1234'});
+        const wrapper = mount(<RegisterForm />);
+        wrapper.setState({ username: 'chris', email: 'chris@gmail.com', password: 'maina1234', cpassword: 'maina1234' });
         const pageForm = wrapper.find('Form')
         pageForm.simulate('submit')
         expect(RegisterForm.prototype.validate.calledOnce).toEqual(true);
