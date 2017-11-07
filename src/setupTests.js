bb0e8328103fd49ad7f576b02515abff08f0c69b
Chris-Maina <krismaish34@gmail.com>
2017-11-07 18:54:58 +0300

    [Chore #[152374386]] Add local storage mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,10 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
 configure({ adapter: new Adapter() });
+const localStorageMock = {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    clear: jest.fn()
+  };
+  global.localStorage = localStorageMock
+  
