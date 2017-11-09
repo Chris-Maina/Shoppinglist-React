9f22253a11819b62340d54e6221502a93d7afb3b
Chris-Maina <krismaish34@gmail.com>
2017-11-09 07:57:44 +0300

    [Chore] Add test cases for ToggleComponent icon clicks

--- src/__tests__/shoppinglist.test.js
+++ src/__tests__/shoppinglist.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import ReactDOM from 'react-dom';
-import  {ShoppinglistPage, Shoppinglist } from '../components/shoppinglist';
-import { Card } from 'react-materialize';
+import  {ShoppinglistPage, Shoppinglist,ShoppinglistForm, ToggleableShoppingForm, SearchShoppinglist, LimitShoppinglists } from '../components/shoppinglist';
 import sinon from 'sinon';
 import moxios from 'moxios';
 import axios from 'axios';
@@ -15,13 +14,12 @@ describe('<ShoppinglistPage/> components', () => {
         moxios.uninstall()
     })
     it('Posts a shoppinglist', () => {
-       
-        const shoppinglist_page = mount(<ShoppinglistPage/>)
-        const form = shoppinglist_page.find('form')
-        const inputName = shoppinglist_page.find("input[name='shoppinglistname']")
+        const shoppinglist_page = shallow(<ShoppinglistForm/>)
+        const form = shoppinglist_page.find('Form')
+        const inputName = shoppinglist_page.find("Input[name='shoppinglistname']")
         const event = {
                     target: {
-                        value : 'Back to school',
+                        value : 'Back to school'
                     },
                     preventDefault: () => {
                         return true
@@ -42,18 +40,44 @@ describe('<ShoppinglistPage/> components', () => {
         })
         
     })
+    
     it('Renders Shoppinglist page by default', ()=>{
         const shoppinglistPage = shallow(<ShoppinglistPage />)
         expect(shoppinglistPage).toHaveLength(1);
     })
     it('Allows to set props', ()=>{
         const shoppinglist = shallow(<Shoppinglist name="Furniture" />)
+        // console.log(shoppinglist)
         expect(shoppinglist.instance().props.name).toEqual("Furniture");
     })
     it('calls componentDidMount', () => {
-        sinon.spy(ShoppinglistPage.prototype, 'componentDidMount');
-        const wrapper = mount(<ShoppinglistPage />);
-        expect(ShoppinglistPage.prototype.componentDidMount.calledOnce).toEqual(true);
+        const testMountCall = sinon.spy(ShoppinglistPage.prototype, 'componentDidMount');
+        const wrapper = shallow(<ShoppinglistPage />);
+        expect(testMountCall.calledOnce).toEqual(true);
       });
 
 })
+describe('ToggleShoppingForm component icon click tests', () =>{
+    it('It opens up ShoppingListForm when Add button is clicked', () =>{
+        const toggleComponent = shallow(<ToggleableShoppingForm/>)
+        const addButton = toggleComponent.find('#add')
+        addButton.simulate('click')
+        expect(ShoppinglistForm).toHaveLength(1)
+
+    })
+    it('It opens up SearchShoppinglist when search button is clicked', () =>{
+        const toggleComponent = shallow(<ToggleableShoppingForm/>)
+        const addButton = toggleComponent.find('#search')
+        addButton.simulate('click')
+        expect(SearchShoppinglist).toHaveLength(1)
+
+    })
+    it('It opens up LimitShoppinglist when search button is clicked', () =>{
+        const toggleComponent = shallow(<ToggleableShoppingForm/>)
+        const addButton = toggleComponent.find('#filter')
+        addButton.simulate('click')
+        expect(LimitShoppinglists).toHaveLength(1)
+
+    })
+})
+
