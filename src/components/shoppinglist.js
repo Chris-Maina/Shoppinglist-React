1b4e8435411d8513bbaae02bcef9d3017478e085
Chris-Maina <krismaish34@gmail.com>
2017-11-19 11:20:07 +0300

    [Chore] Refactor to comment out console messages

--- src/components/shoppinglist.js
+++ src/components/shoppinglist.js
@@ -240,13 +240,14 @@ export class ShoppinglistPage extends Component {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
                 toast.error(error.response.data.message)
-            } else if (error.request) {
-                // The request was made but no response was received
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log('Error', error.message);
-            }
+            } 
+            // else if (error.request) {
+            //     // The request was made but no response was received
+            //     console.log(error.request);
+            // } else {
+            //     // Something happened in setting up the request that triggered an Error
+            //     console.log('Error', error.message);
+            // }
         });
     }
     handleNextClick () {
