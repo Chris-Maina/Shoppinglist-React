98b8296a34126cca105cd7a2f7672e2ba601ff21
Chris-Maina <krismaish34@gmail.com>
2017-11-20 22:28:32 +0300

    [Chore] Refactor to remove navbar and add login button

--- src/components/home.js
+++ src/components/home.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import { Button } from 'react-materialize';
-import { Link } from 'react-router-dom';
 import Footer from './footer';
 import './home.css';
-import {Navigation} from './navbar';
 
 // Home
 class Home extends Component {
   render() {
     return (
       <div>
-        <Navigation />
         <header>
           <div className="header-content">
             <div className="header-content-inner">
               <h1 id="homeHeading"> Shoppinglist Application </h1>
               <hr />
               <p>Shopping list application helps you record all items you wish to buy. It allows you to record and share things you want to spend money on, meeting your needs and
-				        keeping track of your shopping lists. Register to get started!!!
+				        keeping track of your shopping lists.
 			      </p>
-              <Button  className="btn-primary amber accent-4" id="register"> <Link to="/auth/register/">Register</Link></Button>
+            <div>
+            <a href="/auth/register/"><Button className="btn-primary home-buttons amber accent-4"> Register</Button></a>
+            <a href="/auth/login/">  <Button className="btn-primary home-buttons amber accent-4" >Login</Button></a>
+            </div>
             </div>
           </div>
         </header>
