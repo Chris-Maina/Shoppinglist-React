c4b25b6698e5b210a500e9c6e37f2ce1c314924a
Chris-Maina <krismaish34@gmail.com>
2017-11-20 23:01:21 +0300

    [Chore] Refator text in home page

--- src/components/home.js
+++ src/components/home.js
@@ -13,7 +13,7 @@ class Home extends Component {
             <div className="header-content-inner">
               <h1 id="homeHeading"> Shoppinglist Application </h1>
               <hr />
-              <p>Shopping list application helps you record all items you wish to buy. It allows you to record things you want to spend money on, meeting your needs and
+              <p>Shopping list application helps you create, edit or delete shopping list. It allows you to record things or items you want to spend money on, meeting your needs and
 				        keeping track of your shopping lists.
 			      </p>
             <div>
