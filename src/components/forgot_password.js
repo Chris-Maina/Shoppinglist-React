9d99a0260a8e5420605e9a8cdbd63e686b0b22ac
Chris-Maina <krismaish34@gmail.com>
2017-12-13 22:46:59 +0300

    [Chore] Refactor component to fix styling and introduce axios config

--- src/components/forgot_password.js
+++ src/components/forgot_password.js
@@ -4,37 +4,33 @@ import axios from 'axios';
 import { Button, Row, Col, Input, Icon, Card } from 'react-materialize';
 import Form from 'muicss/lib/react/form';
 import { ToastContainer, toast } from 'react-toastify';
-import {LoginForm }from './login';
+import { LoginForm } from './login';
 import './forgot_password.css';
-import {Navigation} from './navbar';
+import { Navigation } from './navbar';
+import axiosConfig from './baseConfig';
 
 class ForgotPassword extends Component {
     constructor(props) {
         super(props);
-        this.state = { redirect: false, email:'' };
+        this.state = { redirect: false, email: '' };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.emailSubmit = this.emailSubmit.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
     }
-    handleInputChange(evt){
+    handleInputChange(evt) {
         evt.preventDefault();
         this.setState({ email: evt.target.value });
     }
-    handleSubmit(evt){
+    handleSubmit(evt) {
         evt.preventDefault();
         this.emailSubmit(this.state.email);
     }
-    emailSubmit(user_email){
+    emailSubmit(user_email) {
         // Send POST request
-        var data = {email: user_email};
-        const url = 'https://shoppinglist-restful-api.herokuapp.com/user/reset';
-        axios({
+        axiosConfig.request({
             method: "post",
-            url: url,
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            data: data
+            url: `/user/reset`,
+            data: { email: user_email }
         }).then((response) => {
             toast.success("Email sent successfully ");
             return response.data;
@@ -63,29 +59,29 @@ class ForgotPassword extends Component {
         return (
             <div>
                 <Navigation />
-            <div className="page_content">
-                <Container fluid={true} >
-                    <ToastContainer />
-                    <Row>
-                        <Col s={12} m={8} offset="m2">
-                            <Card
-                                className="mui--text-center card_forgot_password "
-                                textClassName='teal-text'
+                <div className="page_content">
+                    <Container fluid={true} >
+                        <ToastContainer />
+                        <Row>
+                            <Col s={12} m={8} offset="m2">
+                                <Card
+                                    className="mui--text-center card_forgot_password "
+                                    textClassName='teal-text'
                                 >
-                                <Row>
-                                    <h4>Forgot Password?</h4>
-                                    <span className="black-text">Let's get you a new password ! Just provide your email address below</span><br />
-                                    <Form onSubmit={this.handleSubmit}>
-                                        <Input s={6} label="Email" className="black-text" validate type='email' name="email" onChange={this.handleInputChange}><Icon>email</Icon></Input><br/>
-                                        <Button s={6} size="small" waves='light'>Submit </Button>
-                                    </Form> 
-                                </Row>
+                                    <Row>
+                                        <h4>Forgot Password?</h4>
+                                        <span className="black-text">Let's get you a new password ! Just provide your email address below</span><br />
+                                        <Form onSubmit={this.handleSubmit}>
+                                            <Input s={6} label="Email" className="black-text" validate type='email' name="email" onChange={this.handleInputChange}><Icon>email</Icon></Input><br />
+                                            <Button s={6} size="small" waves='light'>Submit </Button>
+                                        </Form>
+                                    </Row>
 
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
             </div>
         );
     }
